426420eca59164b488a36a2a429f3563b35c4f34
Phillip Winn <pwinn@winnski.com>
2023-12-03 15:03:08 -0600

    Updated day 3 just because

--- 2023/ex3.py
+++ 2023/ex3.py
@@ -4,6 +4,29 @@ import re
 
 gears = {}
 
+def find_gears(schematic):
+    gears = {}
+    for row_num, row in enumerate(schematic):
+        for col_num, char in enumerate(row):
+            if char == '*':
+                gears[(row_num, col_num)] = []
+    return gears
+
+def get_resultA(schematic):
+    result = 0
+    for row_num, row in enumerate(schematic):
+        for match in re.finditer('\d+', row):
+            if symbol_is_adjacent(match.group(0), match.start(), row_num):
+                result += int(match.group(0))
+    return result
+
+def get_resultB(gears):
+    result = 0
+    for gear, vals in gears.items():
+        if len(vals) > 1:
+            result += (int(vals[0]) * int(vals[1]))
+    return result
+
 def symbol_is_adjacent(val, val_x, val_y):
     global gears
     for y in range(val_y-1, val_y+2):
@@ -15,22 +38,10 @@ def symbol_is_adjacent(val, val_x, val_y):
                     return True
     return False
 
-
 if __name__ == '__main__':
     with open('input3.txt') as input:
-        resultA, resultB = 0, 0
         schematic = []
         for line in input.read().splitlines():
             schematic.append(line)
-        for row_num, row in enumerate(schematic):
-            for col_num, char in enumerate(row):
-                if char == '*':
-                    gears[(row_num, col_num)] = []
-        for row_num, row in enumerate(schematic):
-            for match in re.finditer('\d+', row):
-                if symbol_is_adjacent(match.group(0), match.start(), row_num):
-                    resultA += int(match.group(0))
-        for gear, vals in gears.items():
-            if len(vals) > 1:
-                resultB += (int(vals[0]) * int(vals[1]))
-        print(resultA, resultB)
+        gears = find_gears(schematic)
+        print(get_resultA(schematic), get_resultB(gears))
