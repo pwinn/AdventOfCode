5ccde410bf953c1082af4ad1329c5fd5fb92a83a
Phillip Winn <pwinn@winnski.com>
2023-12-11 09:15:13 -0600

    Day 11 cleanup, thanks to ayberk for suggesting I could simplify the loop

--- 2023/ex11.py
+++ 2023/ex11.py
@@ -2,12 +2,11 @@
 
 def distance(factor = 1):
     result = 0
-    for g in galaxies:
-        for o in galaxies:
-            if o[0] > g[0] or (o[0] == g[0] and o[1] > g[1]):
-                down = o[0] - g[0] + sum([factor for r in range(g[0]+1,o[0]) if r in empty_rows])
-                over = max(o[1], g[1]) - min(o[1], g[1]) + sum([factor for c in range(min(g[1],o[1]), max(g[1],o[1])) if c in empty_columns])
-                result += down + over
+    for i, g in enumerate(galaxies):
+        for o in galaxies[i+1:]:
+            down = o[0] - g[0] + sum([factor for r in range(g[0]+1,o[0]) if r in empty_rows])
+            over = max(o[1], g[1]) - min(o[1], g[1]) + sum([factor for c in range(min(g[1],o[1]), max(g[1],o[1])) if c in empty_columns])
+            result += down + over
     return result
 
 if __name__ == '__main__':
